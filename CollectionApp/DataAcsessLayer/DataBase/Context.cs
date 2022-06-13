93f96922adb5055ba351469acc25d21033cb6344
LAPTOP-BG7SOPOU\johnn <johnny6276@gmail.com>
2022-06-13 19:56:19 +0700

    iNITIAL

---
+++ CollectionApp/DataAcsessLayer/DataBase/Context.cs
@@ -0,0 +1,23 @@
+﻿using DataAcsessLayer.DataBase.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcsessLayer.DataBase
+{
+  public  class Context: IdentityDbContext<AppUser>
+    {
+        public Context()
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ CollectionApp/DataAcsessLayer/DataBase/Entities/AppUser.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataAcsessLayer.DataBase.Entities
+{
+   public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastNamee { get; set; }
+        public string FullName{ get; set; }
+        public string Info{ get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public string Country { get; set; }
+
+
+
+
+
+    }
+}
