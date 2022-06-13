4da54e77b1d54d794e0bb91895d8d1fb46f0234e
LAPTOP-BG7SOPOU\johnn <johnny6276@gmail.com>
2022-06-13 20:03:51 +0700

    userconfigadd

---
+++ CollectionApp/DataAcsessLayer/DataBase/EntitiyConfig/UserConfig.cs
@@ -0,0 +1,21 @@
+﻿using DataAcsessLayer.DataBase.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcsessLayer.DataBase.EntitiyConfig
+{
+    class UserConfig : IEntityTypeConfiguration<AppUser>
+    {
+        public void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            builder.Property(x=> x)
+        }
+    }
+}
